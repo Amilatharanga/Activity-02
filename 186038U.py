ef61d27ca4c52928776b79111051201390c203f4
G.G.Amila Tharanga Kumara <92165995+Amilatharanga@users.noreply.github.com>
2021-10-08 20:04:49 +0530

    Edit in github

--- 186038U.py
+++ 186038U.py
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+#code 01
 # Python program to display all the prime numbers within an interval
 
 lower = 1000
@@ -15,6 +16,7 @@ for num in range(lower, upper + 1):
        else:
            print(num)
 =======
+#code 02
 # Program to check Armstrong numbers in a certain interval
 
 lower = 100
